de7ad1c675fc3cb08653443b5941a6702049dc89
Abhid1505 <87860470+Abhid1505@users.noreply.github.com>
2022-01-18 10:03:29 +0530

    Update code1.sql

--- code1.sql
+++ code1.sql
@@ -16,3 +16,30 @@ INSERT INTO MOVIES (Actor,Actress,Director,year,movie) VALUES (
 ); SELECT * FROM MOVIES;
 
 SELECT * from MOVIES where actor="Abhi"
+
+
+
+connection_obj = sqlite3.connect('MOVIES.db')
+ 
+# cursor object
+cursor_obj = connection_obj.cursor()
+ 
+# Drop the GEEK table if already exists.
+cursor_obj.execute("DROP TABLE IF EXISTS MOVIES")
+ 
+# Creating table
+table = """ CREATE TABLE MOVIES (
+            movie name string NOT NULL,
+            Actor name string,
+            Actress name string,
+            Director name string,
+            UNIQUE(YEAR),
+            PRIMARY KEY(year)
+    );
+ 
+cursor_obj.execute(table)
+ 
+print("Table is Ready")
+ 
+# Close the connection
+connection_obj.close()
